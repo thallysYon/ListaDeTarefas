2ef027f5393eae5b4fd7de989624b97c5f035d11
Thallys <104241520+thallysYon@users.noreply.github.com>
2025-11-25 20:21:18 -0300

    Criação dos Enums que serão utilizados no projeto

---
+++ Enums/PrioridadeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListaDeTarefas.Enums
+{
+    public enum PrioridadeEnum
+    {
+        [Display(Name = "Baixa")]
+        Baixa = 0,
+        [Display(Name = "Média")]
+        Media = 1,
+        [Display(Name = "Alta")]
+        Alta = 2
+    }
+}
---
+++ Enums/SituacaoEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ListaDeTarefas.Enums
+{
+    public enum SituacaoEnum
+    {
+        [Display(Name = "Pendente")]
+        Pendente = 1,
+        [Display(Name = "Em Andamento")]
+        EmAndamento = 2,
+        [Display(Name = "Concluida")]
+        Concluida = 3,
+        [Display(Name = "Cancelada")]
+        Cancelada = 4,
+        [Display(Name = "Impedida")]
+        Impedida = 5,
+
+    }
+}
