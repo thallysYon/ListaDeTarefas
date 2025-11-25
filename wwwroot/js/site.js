11191ca89ea2f34e126d6de7cd8adc16ee87dcdf
thallysYon <thallya@gmail.com>
2025-11-25 02:00:01 -0300

    Adicionar arquivos de projeto.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿
+const themeToggle = document.getElementById('btnAlternarTema');
+
+
+function getPreferredTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        return storedTheme;
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+
+function setTheme(theme) {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+
+    const icon = themeToggle.querySelector('i');
+    icon.className = theme === 'dark' ? 'bi bi-sun-fill' : 'bi bi-moon-fill';
+}
+
+
+setTheme(getPreferredTheme());
+
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+});
